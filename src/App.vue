3ea628454662778899d3cc99f903d1f66da107c1
Biswitha <biswithareddy@gmail.com>
2022-07-07 09:18:45 -0500

    User Login

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <v-app>
+    <v-app-bar>
+      <v-app-bar-title>
+        <v-btn variant="text" @click="$router.push('/')"> Survey </v-btn>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn variant="text" @click="$router.push('/login')"> Login </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-items>
+        <v-btn variant="text" @click="$router.push('/signup')"> Signup </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <router-view />
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-}
+  name: "App",
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-
-loadFonts()
+import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "bootstrap"
+import store from "./store"
+import { FontAwesomeIcon } from './plugins/font-awesome'
+//loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(router)
+  .use(store)
+  .component("font-awesome-icon",FontAwesomeIcon)
   .mount('#app')
